a40719d9b7a0edceca4926f3f9c758529b01168f
Vineet <microsoftvars@gmail.com>
2026-07-31 23:16:02 +0530

    Solve DSA

--- objects.js
+++ objects.js
@@ -148,8 +148,96 @@
 // console.log(arr);
 
 
-const arr = [1,2,3];
+// const arr = [1,2,3];
 
-console.log(arr.slice(1,3));
+// console.log(arr.slice(1,3));
 
-console.log(arr);
+// console.log(arr);
+
+
+// function hasDuplicates(nums) {
+//     for (let i = 0; i < nums.length; i++) {
+//         for (let j = i + 1; j < nums.length; j++) {
+//             if (nums[i] === nums[j]) {
+//                  console.log(true);
+//             }
+//         }
+//     }
+//     console.log(false);
+// };
+// hasDuplicates([1,2,3,4])
+
+// let s = "vineet"
+// let t = "eeifvt"
+
+// let sorts = s.split('').sort().join('');
+// console.log(sorts)
+
+// let tsort = t.split('').sort().join('');
+// console.log(tsort)
+
+// if(sorts === tsort){
+//     return console.log(true)
+// }
+// else{
+//     return console.log(false)
+// }
+
+// function isAnegram(s,t){
+//     let ssort = s.split('').sort().join('');
+//     let tsort = t.split('').sort().join('');
+//     if(ssort === tsort){
+//         return true
+//     }else{
+//         return false
+//     }
+
+// }
+// console.log(isAnegram("vineet","eeivnt"));
+
+
+// function isAnagram(s,t){
+//     if(s.length !== t.length){
+//         return false
+//     }
+//     let table = {};
+
+//     for(let i=0;i<s.length;i++){
+//         let char = s[i];
+
+//         if(table[char]){
+//             table[char]++;
+//         }else{
+//             table[char] = 1
+//         }
+//     }
+
+//     for(let i=0;i<t.length;i++){
+//         let char = t[i]
+
+//         if(!table[char]){
+//             return false
+//         }
+//         table[char]--;
+//     }
+//     return true
+// }
+// console.log(isAnagram("vineet","eecvt"))
+
+// let arr =[1,2,3,4];
+// let ans = new Array(arr.length * 2);
+
+
+function getConcatenation(nums) {
+    let ans = [];
+
+    for (let i = 0; i < 2; i++) {
+       
+        for (let num of nums) {
+            ans.push(num)
+            
+        }
+    }
+    return ans
+}
+console.log(getConcatenation([1, 2, 3, 4]))
